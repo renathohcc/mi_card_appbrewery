94f825054aef9a56176edc614575e53ffacd5ae5
renathohcc <henrique.renatho@gmail.com>
2021-10-05 23:39:27 -0300

    Section_6_Class_43

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100.0,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  color: Colors.yellow,
+                  width: 100.0,
+                  height: 100.0,
+                ),
+                Container(
+                  color: Colors.yellow.withOpacity(0.4),
+                  width: 100.0,
+                  height: 100.0,
+                ),
+              ],
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
