c4617809c7b6415c70df51be049a810923c56077
renathohcc <henrique.renatho@gmail.com>
2021-10-06 15:13:00 -0300

    [STABLE] Final Version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,36 +13,96 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepOrange,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: 100.0,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  color: Colors.yellow,
-                  width: 100.0,
-                  height: 100.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/img_perfil.png'),
+              ),
+              Text(
+                'Renatho Henrique',
+                style: TextStyle(
+                  fontFamily: 'ArchDaughter',
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                Container(
-                  color: Colors.yellow.withOpacity(0.4),
-                  width: 100.0,
-                  height: 100.0,
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.deepOrange[50],
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
                 ),
-              ],
-            ),
-            Container(
-              width: 100.0,
-              color: Colors.blue,
-            ),
-          ],
-        )),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.deepOrange[100],
+                  thickness: 1.0,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    '+55 99999-9999',
+                    style: TextStyle(
+                      color: Colors.deepOrange[900],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    'henrique.renatho@gmail.com',
+                    style: TextStyle(
+                      color: Colors.deepOrange[900],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.photo_camera,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    '@renathohcc',
+                    style: TextStyle(
+                      color: Colors.deepOrange[900],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
